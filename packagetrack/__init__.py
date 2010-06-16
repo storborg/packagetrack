147397e1864fe3fd108aca1cfff98c07f59d6ccb
Scott Torborg <scott@crookedmedia.com>
2010-06-16 11:25:14 -0700

    move TrackingInfo() to separate subpackage for ease of imports

--- packagetrack/__init__.py
+++ packagetrack/__init__.py
@@ -28,13 +28,6 @@ class UnsupportedShipper(Exception):
     pass
 
 
-class TrackingInfo(object):
-    
-    def __init__(self, delivery_date, status):
-        self.delivery_date = delivery_date
-        sel.status = status
-
-
 class Package(object):
 
     def __init__(self, tracking_number):
---
+++ packagetrack/data.py
@@ -0,0 +1,5 @@
+class TrackingInfo(object):
+    
+    def __init__(self, delivery_date, status):
+        self.delivery_date = delivery_date
+        sel.status = status
