c9e234bddf2fd296e25bb223c6a6ca72920064b3
Scott Torborg <scott@crookedmedia.com>
2010-06-21 13:12:28 -0700

    added tracking url generation for fedex and USPS

--- packagetrack/fedex.py
+++ packagetrack/fedex.py
@@ -2,3 +2,10 @@ class FedexInterface(object):
 
     def identify(self, tracking_number):
         return len(tracking_number) == 15
+
+    def track(self, tracking_number):
+        raise NotImplementedError
+
+    def url(self, tracking_number):
+        return ('http://www.fedex.com/Tracking?tracknumbers=%s'
+                % tracking_number)
--- packagetrack/usps.py
+++ packagetrack/usps.py
@@ -2,3 +2,7 @@ class USPSInterface(object):
 
     def identify(self, tracking_number):
         return tracking_number.startswith('91')
+
+    def url(self, tracking_number):
+        return ('http://trkcnfrm1.smi.usps.com/PTSInternetWeb/'
+                'InterLabelInquiry.do?origTrackNum=%s' % tracking_number)
