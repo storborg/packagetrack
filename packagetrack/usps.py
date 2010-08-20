b70c2d81753cd1c779e77ca32bce34ada2e859f2
Scott Torborg <scott@crookedmedia.com>
2010-08-20 13:28:00 -0700

    usps tracking numbers also start with 94

--- packagetrack/usps.py
+++ packagetrack/usps.py
@@ -1,7 +1,8 @@
 class USPSInterface(object):
 
     def identify(self, tracking_number):
-        return tracking_number.startswith('91')
+        return (tracking_number.startswith('91') or
+                tracking_number.startswith('94'))
 
     def url(self, tracking_number):
         return ('http://trkcnfrm1.smi.usps.com/PTSInternetWeb/'
